b256e755a25c127b1524854e76b41a9d20130127
ProsperoR <61214914+prosperor@users.noreply.github.com>
2020-10-21 17:30:52 -0200

    Inserção do esqueleto de projeto
    Item conforme issue #17

---
+++ br/unifil/dc/sisop/ComandoPrompt.java
@@ -0,0 +1,19 @@
+package br.unifil.dc.sisop;
+
+public class ComandoPrompt {
+    
+    private final String nome;
+    private final String[] argumentos;
+
+    public ComandoPrompt(String comando) {
+        
+    }
+
+    public String getNome() {
+        
+    }
+
+    public List<String> getArgumentos() {
+        
+    }
+}
