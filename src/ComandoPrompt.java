e75651636ee22be0e2e4c74dd01c28efe8334927
ProsperoR <61214914+prosperor@users.noreply.github.com>
2020-10-22 01:32:11 -0200

    Classe ComandoPrompt devidamente criada e instânciada
    Fechamento da issue #4

--- src/ComandoPrompt.java
+++ src/ComandoPrompt.java
@@ -1,20 +1,21 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ComandoPrompt {
-
     private final String nome;
     private final String[] argumentos;
 
     public ComandoPrompt(String comando) {
-        nome = "";
-        argumentos = null;
+        this.nome = comando.contains(" ") ? comando.substring(0, comando.indexOf(32)).toLowerCase() : comando.toLowerCase();
+        this.argumentos = comando.contains(" ") ? comando.replace(this.nome, "").split(" ") : null;
     }
 
     public String getNome() {
-        return "";
+        return this.nome;
     }
 
     public List<String> getArgumentos() {
-        return null;
+        return Collections.unmodifiableList(Arrays.asList(this.argumentos));
     }
 }
