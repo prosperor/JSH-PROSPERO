7e69f3220d6497b59f33efa4defa239effc2fc0e
prosperor <prospero@edu.unifil.br>
2020-11-12 14:01:15 -0300

    Ajustes

---
+++ src/main/java/All/App.java
@@ -0,0 +1,13 @@
+package All;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/All/ComandoPrompt.java
@@ -0,0 +1,22 @@
+package All;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ComandoPrompt {
+    private final String nome;
+    private final String[] argumentos;
+
+    public ComandoPrompt(String comando) {
+        this.nome = comando.contains(" ") ? comando.substring(0, comando.indexOf(32)).toLowerCase() : comando.toLowerCase();
+        this.argumentos = comando.contains(" ") ? comando.replace(this.nome, "").split(" ") : null;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public List<String> getArgumentos() {
+        return Collections.unmodifiableList(Arrays.asList(this.argumentos));
+    }
+}
